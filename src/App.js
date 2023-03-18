cfc4fbaa32a565fe2912a95aee55aec541dbfb9c
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2023-03-18 21:07:14 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
-    <div >
-    <h1>Hello</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
