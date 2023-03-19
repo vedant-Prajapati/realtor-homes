4d245ced3dbbd80ec854e564f3879bb71b7104ea
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2023-03-19 16:53:17 +0530

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const Header = () => {
+  const location = useLocation()
+
+  const navigate = useNavigate()
+
+  const pathMathRoute = (route) => {
+    if (route === location.pathname) {
+      return true
+    }
+  }
+  return (
+    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+      <header className='flex justify-between items-center px-20 mx-w-6xl mx-auto'>
+        <div>
+          <img src="./Images/logo.svg" alt="logo" className='h-5 cursor-pointer' onClick={() => navigate("/")} />
+        </div>
+        <div>
+          <ul className='flex space-x-10'>
+            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/")}>Home</li>
+            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/offers")}>Offers</li>
+            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/sign-in")}>Sign in</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
