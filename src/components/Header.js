b34f519bdd8c282e2dd7a9564e46d27541334770
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2023-03-27 15:17:30 +0530

    complete the header  component and make it dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import {getAuth, onAuthStateChanged} from "firebase/auth"
 
 const Header = () => {
+  const [pageState, setPageState] = useState("Sign in") 
   const location = useLocation()
-
   const navigate = useNavigate()
-
-  const pathMathRoute = (route) => {
+  const auth = getAuth()
+  useEffect(()=> {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile")
+      }else{
+        setPageState("Sign in")
+      }
+    })
+  }, [auth])
+  const pathMatchRoute = (route) => {
     if (route === location.pathname) {
       return true
     }
@@ -19,9 +29,9 @@ const Header = () => {
         </div>
         <div>
           <ul className='flex space-x-10'>
-            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/")}>Home</li>
-            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/offers")}>Offers</li>
-            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/sign-in")}>Sign in</li>
+            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/")}>Home</li>
+            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("/offers")}>Offers</li>
+            <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-red-700 border-b-red-500"}`} onClick={() =>navigate("profile")}>{pageState}</li>
           </ul>
         </div>
       </header>
