e95b532ddcf2cbfc3602875db18a875a1f840a70
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2023-03-29 16:54:41 +0530

    create EditListing page

--- src/App.js
+++ src/App.js
@@ -10,22 +10,26 @@ import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing"
 function App() {
-  return (  
+  return (
     <>
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={< PrivateRoute/>}>
+          <Route path="/profile" element={< PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={< PrivateRoute/>}>
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-listing" element={< PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="/edit-listing" element={< PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
