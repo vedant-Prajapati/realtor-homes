de230816d957884cca2d0dc54b565a3c9163a22a
Vedant coding <100594881+Mrvedant99@users.noreply.github.com>
2023-04-09 21:15:08 +0530

    Create the landlord contact component

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -12,12 +12,16 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa"
+import { getAuth } from "firebase/auth";
+import Contact from '../components/Contact'
 
 const Listing = () => {
+    const auth = getAuth()
     const params = useParams()
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
     const [shareLinkCopied, setShareLinkCopied] = useState(false)
+    const [contactLandlord, setContactLandlord] = useState(false);
     SwiperCore.use([Autoplay, Navigation, Pagination])
     useEffect(() => {
         async function fetchListing() {
@@ -62,8 +66,8 @@ const Listing = () => {
                 <FaShare className=' text-lg text-slate-500' />
             </div>
             {shareLinkCopied && (<p className=' fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied </p>)}
-            <div className="m-4 flex flex-col md:flex-row max-w-6px lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-                <div className="w-full h-[200px] lg-[400px]">
+            <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+                <div className="w-full">
                     <p className='text-2xl font-bold mb-3 text-blue-900'>
                         {listing.name} - ${listing.offer ? listing.discountedPrice.toString()
                             .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString()
@@ -87,7 +91,7 @@ const Listing = () => {
                         <span className="font-semibold">Description - </span>
                         {listing.description}
                     </p>
-                    <ul className='flex items-center space-x-2 sm:space-x-10 text-sm font-semibold '>
+                    <ul className='flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6'>
                         <li className='flex items-center whitespace-nowrap'>
                             <FaBed className='text-lg mr-1'/>
                         {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
@@ -108,7 +112,16 @@ const Listing = () => {
                         {listing.furnished > 1 ? "Furnished " : "Not Furnished"}
                         </li>
                     </ul>
-
+                    {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+                    <div className="mt-6">
+                    <button onClick={()=>setContactLandlord(true)} className='px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out'>
+                        Contact Landlord
+                        </button>
+                    </div>
+                    )}
+                    {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
                 </div>
                 <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
             </div>
